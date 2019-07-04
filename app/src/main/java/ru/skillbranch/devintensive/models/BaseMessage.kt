1390df33a22f4a0c078a32708621ad35b98b1eb0
rjumko <rjumko@gmail.com>
2019-07-04 17:05:58 +0700

    add BaseMessage, Chat

---
+++ app/src/main/java/ru/skillbranch/devintensive/models/BaseMessage.kt
@@ -0,0 +1,12 @@
+package ru.skillbranch.devintensive.models
+
+import java.util.*
+
+abstract class BaseMessage(
+    val id: String,
+    val from: User?,
+    val chat: Chat,
+    val isIncoming: Boolean,
+    val date: Date = Date()
+) {
+}
