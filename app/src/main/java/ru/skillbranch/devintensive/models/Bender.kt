264f688d1ce61fc023930bd20f7c7985d18f1183
rjumko <rjumko@gmail.com>
2019-07-21 14:56:41 +0700

    add bender class model

--- app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -2,19 +2,29 @@ package ru.skillbranch.devintensive.models
 
 class Bender(var status: Status = Status.NORMAL, var question: Question = Question.NAME) {
 
-    fun askQuestion(): String {
-        TODO()
+    fun askQuestion(): String = when (question) {
+        Question.NAME -> Question.NAME.question
+        Question.PROFESSION -> Question.PROFESSION.question
+        Question.MATERIAL -> Question.MATERIAL.question
+        Question.BDAY -> Question.BDAY.question
+        Question.SERIAL -> Question.SERIAL.question
+        Question.IDLE -> Question.IDLE.question
     }
 
     fun listenAnswer(answer: String): Pair<String, Triple<Int, Int, Int>> {
-        TODO()
+
+        return if(question.answer.contains(answer)) {
+            "Отлично - это правильный ответ!" to status.color
+        }else{
+            "Это не провильный ответ!" to status.color
+        }
     }
 
-    enum class Status {
-        NORMAL,
-        WARNING,
-        DANGER,
-        CRITICAL,
+    enum class Status(val color: Triple<Int, Int, Int>) {
+        NORMAL(Triple(255, 255, 255)),
+        WARNING(Triple(255, 120, 0)),
+        DANGER(Triple(255, 60, 60)),
+        CRITICAL(Triple(255, 255, 0)),
     }
 
     enum class Question(val question: String, val answer: List<String>) {
