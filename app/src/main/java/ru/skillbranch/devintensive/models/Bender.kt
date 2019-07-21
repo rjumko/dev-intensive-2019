8118e64c53c8647646a42cb0f57b68c92946d41a
rjumko <rjumko@gmail.com>
2019-07-21 13:40:49 +0700

    add bender class model

--- app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -2,20 +2,28 @@ package ru.skillbranch.devintensive.models
 
 class Bender(var status: Status = Status.NORMAL, var question: Question = Question.NAME) {
 
-    enum class Status{
+    fun askQuestion(): String {
+        TODO()
+    }
+
+    fun listenAnswer(answer: String): Pair<String, Triple<Int, Int, Int>> {
+        TODO()
+    }
+
+    enum class Status {
         NORMAL,
         WARNING,
         DANGER,
         CRITICAL,
     }
 
-    enum class Question{
-        NAME,
-        PROFESSION,
-        MATERIAL,
-        BDAY,
-        SERIAL,
-        IDLE,
+    enum class Question(val question: String, val answer: List<String>) {
+        NAME("Как меня зовут?", listOf("бендер", "bender")),
+        PROFESSION("Назови мою профессию?", listOf("сгибальщик", "bender")),
+        MATERIAL("Из чего я сделан?", listOf("метал", "дерево", "metal", "iron", "wood")),
+        BDAY("Когда меня создали?", listOf("2993")),
+        SERIAL("Мой серийный номер?", listOf("2716057")),
+        IDLE("На этом все, вопросов больше нет", listOf()),
     }
 
 }
