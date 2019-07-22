54bc4ae341f0e9cc691c77dca378652bb9312a84
rjumko <rjumko@gmail.com>
2019-07-22 21:38:39 +0700

    bender

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
@@ -2,13 +2,41 @@ package ru.skillbranch.devintensive.extensions
 
 import android.app.Activity
 import android.content.Context
+import android.graphics.Rect
+import android.view.View
 import android.view.inputmethod.InputMethodManager
 
 fun Activity.hideKeyboard() {
-    val inputManager = this?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-    val focusedView = this?.currentFocus
+    val inputManager = this.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    val focusedView = this.currentFocus
     if (focusedView != null) {
-        inputManager.hideSoftInputFromWindow(focusedView.windowToken,
-            InputMethodManager.HIDE_NOT_ALWAYS)
+        inputManager.hideSoftInputFromWindow(
+            focusedView.windowToken,
+            InputMethodManager.HIDE_NOT_ALWAYS
+        )
     }
+}
+
+fun Activity.isKeyboardOpen(): Boolean {
+
+
+    val rect = Rect()
+    val rootView = this.window.decorView
+    rootView.getWindowVisibleDisplayFrame(rect)
+    val visibleDisplayFrameHeight = rect.top
+    val screenHeight = this.windowManager.defaultDisplay.height
+    val diff = (screenHeight - visibleDisplayFrameHeight)
+    return diff > 100
+
+}
+
+fun Activity.isKeyboardClosed(): Boolean {
+
+    val rect = Rect()
+    val rootView = this.window.decorView
+    rootView.getWindowVisibleDisplayFrame(rect)
+    val visibleDisplayFrameHeight = rect.top
+    val screenHeight = this.windowManager.defaultDisplay.height
+    val diff = (screenHeight - visibleDisplayFrameHeight)
+    return diff < 100
 }
