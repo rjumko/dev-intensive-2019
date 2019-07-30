53b1c5f5c13a542b428aa26a77243763f40bd4d5
rjumko <rjumko@gmail.com>
2019-07-30 18:42:48 +0700

    add AspectRationImageView

---
+++ app/src/main/java/ru/skillbranch/devintensive/ui/custom/AspectRatioImageView.kt
@@ -0,0 +1,13 @@
+package ru.skillbranch.devintensive.ui.custom
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.ImageView
+
+class AspectRatioImageView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : ImageView(context, attrs, defStyleAttr) {
+}
+
