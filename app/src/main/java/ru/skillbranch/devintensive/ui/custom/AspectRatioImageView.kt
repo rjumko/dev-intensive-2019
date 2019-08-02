a96aaf2f509d3d4934e8d55c624c2984aa523d01
rjumko <rjumko@gmail.com>
2019-08-02 17:51:20 +0700

    add AspectRationImageView

--- app/src/main/java/ru/skillbranch/devintensive/ui/custom/AspectRatioImageView.kt
+++ app/src/main/java/ru/skillbranch/devintensive/ui/custom/AspectRatioImageView.kt
@@ -9,5 +9,15 @@ class AspectRatioImageView @JvmOverloads constructor(
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
 ) : ImageView(context, attrs, defStyleAttr) {
+    companion object{
+        private const val DEFAULT_ASPECT_RATIO = 1.78f
+    }
+
+    private var aspectRatio = DEFAULT_ASPECT_RATIO
+
+    init{
+
+    }
+
 }
 
