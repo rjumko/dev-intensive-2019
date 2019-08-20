1ae877102399f2a1e5960a2b6ffb95a134e8e35c
rjumko <rjumko@gmail.com>
2019-08-20 17:14:25 +0700

    add button background, edit icon, day/night icon.

--- app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
@@ -22,6 +22,7 @@ class ProfileActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
 
     }
 
